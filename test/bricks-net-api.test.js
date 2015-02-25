093b17af5190b9d0218bd4e26858d28fd9bf0240
John Babak <babak.john@gmail.com>
2015-02-25 04:51:40 +0300

    Implementing Bricks REPL (work in progress)
    Moved to promise-based approach because blocking operations are not
    possible in Node.js without native support for blocking operations
    (there is a blocking file system I/O but there are no blocking sockets
    I/O, timers etc.). The callbacks do not get processed by the JS Event
    Loop until the currently executing function (that waits for a certain
    condition via sleep) finishes and unwinds the stack. But if it
    finishes, the blocking context is lost. For now, there is still much to
    be re-implemented.

--- test/bricks-net-api.test.js
+++ test/bricks-net-api.test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var when = require('when');
 
 describe('bricks-net-api', function () {
 	var api = require('../lib/bricks-net-api');
@@ -19,6 +20,10 @@ describe('bricks-net-api', function () {
 		assert.equal('function', typeof api.HTTP);
 	});
 	
+	it('should export `HTTPResponse` function', function () {
+		assert.equal('function', typeof api.HTTPResponse);
+	});
+	
 	describe('`GET` function', function () {
 		it('should return an instance of `GET` class', function () {
 			assert.equal(true, api.GET("url") instanceof api.GET);
@@ -34,6 +39,53 @@ describe('bricks-net-api', function () {
 	describe('`POSTFromFile` function', function () {
 		it('should return an instance of `POSTFromFile` class', function () {
 			assert.equal(true, api.POSTFromFile("url", __filename, "application/javascript") instanceof api.POSTFromFile);
+		});
+	});
+	
+	describe('`HTTP` function', function () {
+		var http = require('http');
+		var server;
+		var sockets = [];
+		
+		beforeEach(function (done) {
+			server = http.createServer();
+			server.on('connection', function (socket) {
+				sockets.push(socket);
+				socket.on('close', function () {
+					var index = sockets.indexOf(socket);
+					if (index >= 0) {
+						sockets.splice(index, 1);
+					}
+				});
+			});
+			server.on('request', function (request, response) {
+				response.end('OK');
+			});
+			server.on('listening', function () {
+				done();
+			});
+			server.listen(20000);
+		});
+		afterEach(function (done) {
+			server.close(function () {
+				done();
+			});
+			while (sockets.length > 0) {
+				sockets.shift().destroy();
+			}
+		});
+		
+		it('should make a `GET` request', function (done) {
+			var response = api.HTTP(api.GET("localhost:20000/test"));
+			
+			assert.equal(true, when.isPromiseLike(response));
+			
+			when(response).then(function (response) {
+				assert.equal(false, when.isPromiseLike(response));
+				assert.equal(true, response instanceof api.HTTPResponse);
+				assert.equal('OK', response.body);
+				done();
+			}).done(void 0, done);
 		});
 	});
 });
--- test/bricks-net-url.test.js
+++ test/bricks-net-url.test.js
@@ -2,6 +2,21 @@ var assert = require('assert');
 
 describe('bricks-net-url', function () {
 	var URL = require('../lib/bricks-net-url').URL;
+	var EmptyURLException = require('../lib/bricks-net-url').EmptyURLException;
 	
-	// TODO(sompylasar): Add tests for `URL`. 
+	it('should export `URL` function', function () {
+		assert.equal('function', typeof URL);
+	});
+	
+	it('should export `EmptyURLException` function', function () {
+		assert.equal('function', typeof EmptyURLException);
+	});
+	
+	describe('`URL` function', function () {
+		it('should throw `EmptyURLException` on empty url', function () {
+			assert.throws(function () {
+				URL("");
+			}, EmptyURLException);
+		});
+	});
 });
