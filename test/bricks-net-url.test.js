7efbbe94e05da6a37003e38c53cef1109361af48
John Babak <babak.john@gmail.com>
2015-02-26 00:50:26 +0300

    Run C++ tests for URL from JS test runner

--- test/bricks-net-url.test.js
+++ test/bricks-net-url.test.js
@@ -13,10 +13,34 @@ describe('bricks-net-url', function () {
 	});
 	
 	describe('`URL` function', function () {
-		it('should throw `EmptyURLException` on empty url', function () {
-			assert.throws(function () {
-				URL("");
-			}, EmptyURLException);
+		// Read the C++ tests for `URL`.
+		var cppTest = require('fs').readFileSync(__dirname + '/bricks-net-url.test.cc');
+		
+		// Convert C++ tests to JS tests.
+		cppTest = String(cppTest);
+		
+		cppTest = cppTest.replace(/^\s*#include.*$/gm, '');
+		cppTest = cppTest.replace(/^\s*using .*$/gm, '');
+		
+		cppTest = cppTest.replace(/TEST\([a-zA-Z0-9_]+, ([a-zA-Z0-9_]+)\) \{/gm, function (m, testName) {
+			return "it('" + testName + "', _test_" + testName + "); function _test_" + testName + "() {";
 		});
+		
+		cppTest = cppTest.replace(/EXPECT_EQ\("((?:[^"\\]|\\.)*)", ([a-zA-Z0-9_]+)\.query\["((?:[^"\\]|\\.)*)"\]\);/gm, 'assert.equal("$1", $2.query.get("$3"));');
+		
+		cppTest = cppTest.replace(/EXPECT_EQ\(/gm, 'assert.equal(');
+		cppTest = cppTest.replace(/ASSERT_THROW\((.+?), ([a-zA-Z0-9_]+)\);/gm, 'assert.throws(function () { $1; }, $2);');
+		cppTest = cppTest.replace(/URL u;/gm, 'var u;');
+		cppTest = cppTest.replace(/URL u\(/gm, 'var u = URL(');
+		
+		/* DEBUG *
+		// Print the source code we're going to evaluate, with line numbers for debugging.
+		console.log(cppTest.split('\n').map(function (line, index) {
+			return ((index+1) + '| ' + line);
+		}).join('\n'));
+		// */
+		
+		// Run the C++ tests.
+		eval(cppTest);
 	});
 });
