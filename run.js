396512b7f4140a19c7d6dd50212c13b41500f529
John Babak <babak.john@gmail.com>
2015-03-04 05:41:03 +0300

    Added support for async property access
    * Transform the provided source code to handle promises (via parsing
    and AST traversal).
    * Blacklist JavaScript-specific code constructs (a bonus to AST
    traversal).
    * Blacklist JavaScript-specific global variables (not via AST yet,
    TODO).

--- run.js
+++ run.js
@@ -11,23 +11,14 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', function (buf) { input += buf; });
 process.stdin.on('end', function () {
 	var evaluate = require('./lib/bricks-evaluate');
-	var timing = require('./lib/bricks-timing')();
 	
-	evaluate(input, function (err, result) {
-		timing.end();
-		
+	evaluate(input, function (err, result, timing) {
 		var prettyprint = require('./lib/bricks-prettyprint');
 		
 		var options = {
 			terminal: !!process.stdout.isTTY,
 			useColors: require('chalk').supportsColor
 		};
-		
-		// Treat `undefined` result as an error.
-		// TODO(sompylasar): Think up something for the APIs that return void.
-		if (!err && typeof result === 'undefined') {
-			err = new Error('The result is `undefined`.');
-		}
 		
 		if (err) {
 			process.stderr.write(prettyprint(input, err, timing, options));
